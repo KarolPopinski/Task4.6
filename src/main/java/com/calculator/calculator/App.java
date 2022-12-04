0f1fe54f481fe6f1123a373f67b1df11c6b01773
Karol Popinski <k.popinski@gmail.com>
2022-12-04 19:33:47 +0100

    First Commit

---
+++ src/main/java/com/calculator/calculator/App.java
@@ -0,0 +1,11 @@
+package com.calculator.calculator;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        Calculator calculatorBasic = new Calculator();
+        calculatorBasic.add(10, 10);
+        calculatorBasic.subtract(100, 50);
+    }
+}
---
+++ src/main/java/com/calculator/calculator/Calculator.java
@@ -0,0 +1,13 @@
+package com.calculator.calculator;
+
+public class Calculator {
+
+    public void add(int number1, int number2) {
+        int addResult = number1 + number2;
+        System.out.println("Add result is: " + addResult);
+    }
+    public void subtract(int number1, int number2) {
+        int subtractResult = number1 - number2;
+        System.out.println("Sub result is: " + subtractResult);
+    }
+}
